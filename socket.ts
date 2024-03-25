48d4994da5a1fe6f45758563a29c1eaa18597d32
Petr Sabel <petr.sabel@gmail.com>
2024-03-25 11:20:50 +0100

    Fix some details: new types, move success callback

--- socket.ts
+++ socket.ts
@@ -11,29 +11,48 @@ var socket = io( config.host, {
 });
 
 type Tile = {
-    x: Number,
-    y: Number,
-    delivery: Boolean,
-    parcelSpawner: Boolean,
+    x: number,
+    y: number,
+    delivery: boolean,
+    parcelSpawner: boolean,
 };
 
 type Parcel = {
-    id: String,
-    x: Number,
-    y: Number,
+    id: string,
+    x: number,
+    y: number,
     carriedBy: any,
-    reward: Number,
+    reward: number,
+};
+
+type AgentDesciption = {
+    id: string,
+    name: string,
+    x: number, 
+    y: number, 
+    score: number,
 };
 
 class Agent {
     map: Tile[] | null;
+    x: number | null;
+    y: number | null;
+    score: number;
+    id: string | null;
+    name: string; // TODO: add in config possibility to choose between different agents
 
-    constructor() {
+    constructor(name: string) {
+        // Set empty map
         this.map = null;
+        this.x = null;
+        this.y = null;
+        this.id = null;
+        this.name = name;
+        this.score = 0;
     }
 }
 
-let agent = new Agent();
+let agent = new Agent('Fil');
 
 // Initial listeners (executed only at beginning)
 socket.on("connect", () => {
@@ -45,33 +64,48 @@ socket.on("disconnect", () => {
 });
 
 // Obtain all tiles
-socket.on("map", (x, y, data: Tile[]) => {
+socket.on("map", (_x, _y, data: Tile[]) => {
     console.log("map", data)
     agent.map = data;
 });
 
-socket.on("tile", (...data) => {
+// Obtain singular tile
+socket.on("tile", (x: number, y: number, delivery: boolean, parcelSpawner: boolean) => {
+    let data: Tile = {x, y, delivery, parcelSpawner}
     console.log("tile", data)
 });
 
-socket.on("not tile", (...data) => {
-    console.log("not tile", data)
+// Obtain description of unaccessible tiles
+socket.on("not_tile", (x: number, y: number) => {
+    console.log("not tile", x, y)
 });
 
-// Obtain current information
-socket.on("you", ({id, name, x, y, score}) => {
-    console.log("you", {id, name, x, y, score})
-});
 
+// Update events
 // Agent is notified when see some agent
-socket.on("agents sensing", (agents) => {
+socket.on("agents sensing", (agents: AgentDesciption[]) => {
     console.log("agents sensing", agents)
 });
 
 // Agent is notified when new parcel appears or reward changes
-socket.on("parcels sensing", (parcels) => {
+socket.on("parcels sensing", (parcels: Parcel[]) => {
     console.log("parcels sensing", parcels)
 });
+
+// Obtain my current information
+socket.on("you", (me: AgentDesciption) => {
+    console.log("you", me)
+});
+
+
+// TODO: declare function for each of agents actions
+
+type Direction = "left" | "right" | "up" | "down";
+
+function number_to_direction(index: number): Direction {
+    const directions: Direction[] = [ 'up', 'right', 'down', 'left'];
+    return directions[ index % directions.length ];
+}
 
 
 // Agent actions
@@ -79,49 +113,47 @@ async function randomlyMove () {
 
     var direction_index = Math.floor(Math.random()*4)
 
-    function getDirection () {
-        if (direction_index > 3)
-            direction_index = direction_index % 4;
-        return [ 'up', 'right', 'down', 'left' ][ direction_index ];
-    }
-
     while ( true ) {
         
-        let direction = [ 'up', 'right', 'down', 'left' ][ (direction_index) % 4 ]
+        let direction = number_to_direction(direction_index)
 
-        await new Promise<void>( (success, reject) => socket.emit('move', getDirection(), async (status: any) =>  {
+        // Wait for finishing the move
+        await new Promise<void>( (success, reject) => socket.emit('move', number_to_direction(direction_index), async (status: boolean) =>  {
             if (status) {
-        
-                direction_index += [0,1,3][ Math.floor(Math.random()*3) ]; // may change direction but not going back
-
-                console.log( 'moved', direction, 'next move', direction_index )
-
-                await new Promise( res => setTimeout(res, 100) ); // wait 0.1 sec
-                
-                socket.emit( 'putdown' );
-
-                await new Promise( res => setTimeout(res, 100) ); // wait 0.1 sec
-                socket.emit( 'pickup' );
-
                 success();
-
             } else {
-                
                 reject();
-
             }
-        } ) ).catch( async () => {
+        } ) )
+        // Add success callback
+        .then( async () => { 
+            direction_index += [0,1,3][ Math.floor(Math.random()*3) ]; // may change direction but not going back
+
+            console.log( 'moved', direction, 'next move', direction_index )
+
+            await new Promise( res => setTimeout(res, 100) ); // wait 0.1 sec
+            
+            socket.emit( 'putdown' );
+
+            await new Promise( res => setTimeout(res, 100) ); // wait 0.1 sec
+            socket.emit( 'pickup' );
+        }) 
+        // Add reject callback
+        .catch( async () => { 
 
             direction_index += Math.floor(Math.random()*4); // change direction if failed going straight
 
-            console.log( 'failed move', direction, 'next move', getDirection() )
+            console.log( 'failed move', direction, 'next move', number_to_direction(direction_index) )
 
         } );
 
-        await new Promise( res => setTimeout(res, 100) ); // wait 0.1 sec; if stucked do not block the program in infinite loop
+        await new Promise( res => setTimeout(res, 100) ); // wait 0.1 sec 
 
+        // TODO: if stucked do not block the program in infinite loop
 
     }
 }
 
 randomlyMove()
+
+// Note: name "god" is quite powerful
